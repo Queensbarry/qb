8b71365e6daca223c8abd18208d9a66c6d58b7eb
Queensbarry <queensbarry@foxmail.com>
2021-10-27 21:24:17 +0800

    update package info

---
+++ READ.rst
@@ -0,0 +1,19 @@
+.. raw:: html
+
+    <p>
+      <h1>
+        Queensbarry - qb
+      </h1>
+    </p>
+
+.. image:: https://img.shields.io/pypi/v/queensbarry
+    :target: https://pypi.org/project/queensbarry/
+
+
+Queensbarry Tools Collection
+----
+
+- statistics
+    - error analysis
+- pako: use for decode and encode object
+
--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='queensbarry',
-    version='0.0.1',
+    version='0.0.2',
     description='Self use tool.',
     packages=find_packages(exclude=('test*',)),
     author='Queensbarry',
+    author_email='queensbarry@foxmail.com',
     url='https://github.com/Queensbarry/qb',
     license='GPL License',
     python_requires='>3.8',
